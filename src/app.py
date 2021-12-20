5b6c9bbf7e1f94aba14633534845390d9796cece
AhmedYounes94 <ahmed.hamadino1994@gmail.com>
2021-12-20 00:59:51 +0000

    cleaning

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import os
 from utils import decode_text, current_milli_time
-from data_utils import get_question_indices, NERSample
+from data_utils import  NERSample
 import argparse
 import scipy
 from scipy.special import softmax, log_softmax
@@ -166,13 +166,9 @@ def get_attention():
     attention_summary = extract_attention_summary(attention_tokens[layer_nr][head_nr], features.annotated_tokens)
     local_global = extract_attention_local(attention_tokens[layer_nr][head_nr], features.annotated_tokens)
 
-    # head_similarity = []
-    # heads_similarity = []
-    # for layer in attentions:
-    #     for head in layer[0]:
+
 
     head_similarity = compute_head_similarity(focus, attentions[layer_nr][0][head_nr], features)
-        # head_similarity = []
 
     fig = px.imshow(attention_tokens[layer_nr][head_nr],
                     labels=dict(x="Attend To That Token", y="This Token", color="Attention Weight"),
@@ -225,7 +221,6 @@ def get_impact():
 
     output = {
         'change_heatmap': change_heatmap,
-        # 'embedding_similarity': embedding_similarities
     }
 
     return jsonify(output)
@@ -268,7 +263,6 @@ def generate_model_output(sample, model_name):
 
 
 def compute_head_similarity(focus, head, features):
-    layer_similarities = []
     labels = []
     similarity = []
     distance = []
@@ -284,11 +278,9 @@ def compute_head_similarity(focus, head, features):
     return {'labels':labels , 'similarity':similarity, 'distance': distance }
 
 def compute_similarity(focus, layer, features):
-    layer_similarities = []
     labels = []
     similarity = []
     distance = []
-    # for layer in hidden_states:
     for t, l in zip(features.annotated_tokens.tolist(), layer[:len(features.annotated_tokens)]):
         focus_vector = layer[features.annotated_tokens.tolist().index(focus)]
         if t != focus:
@@ -296,8 +288,6 @@ def compute_similarity(focus, layer, features):
             labels.append(t)
             similarity.append(1 - cosine(focus_vector, token_vector))
             distance.append(cosine(focus_vector, token_vector))
-
-
     return {'labels':labels , 'similarity':similarity, 'distance': distance }
 
 
@@ -313,9 +303,6 @@ def extract_reduced_layers(layers, features):
 
         pca_result_x = layer_reduced[0][len(tokens) * i:len(tokens) * i + len(tokens)]
         pca_result_y = layer_reduced[1][len(tokens) * i:len(tokens) * i + len(tokens)]
-
-        # build json with point information
-        # TODO here they build the poistion infromation for each token all you need to do is to provide slices that distinguish different tags then map that in the js file
         points = []
         for i, val in enumerate(pca_result_x):
             point = {
@@ -332,7 +319,6 @@ def extract_attention_head(token_vectors, features):
     tokens = features.tokens
     reduced_heads = []
     layers = []
-    # token_vectors = [layer[0][:len(tokens)] for layer in layers]
     for heads in token_vectors:
         # unstakc the tensor from 12 seq len seq len to list of seq len seq len then cat
         # the input is 12 48 48 which means for each word we have 48 columns represent it but when we flatten them it becomes 576
@@ -358,15 +344,11 @@ def extract_attention_head(token_vectors, features):
             reduced_heads.append(points)
         layers.append(reduced_heads)
         reduced_heads = []
-    # TODO here they build the poistion infromation for each token all you need to do is to provide slices that distinguish different tags then map that in the js file
     return layers
 def extract_attention_summary(head, annotated_tokens):
     labels = []
     produced_weights = []
     recieved_weights = []
-
-    head_average = np.average(np.array(head))
-
     def get_row(_list, row_idx):
         return sum(np.array(_list[row_idx]))
 
@@ -390,16 +372,10 @@ def extract_attention_local(head, annotated_tokens):
     global_att = []
     global_att_weight = []
 
-    head_average = np.average(np.array(head))
-
     def get_row(_list, row_idx):
         return np.array(_list[row_idx])
 
-    def get_col(_list, col_idx):
-        return np.array(_list[:, col_idx])
-
     for i,  token in enumerate(annotated_tokens):
-        weight = 0
         if token.split('_')[-1] != '-100' and token != '[CLS]' and token != '[SEP]':
             labels.append(token)
             produced_weights = get_row(head, i)
@@ -412,9 +388,6 @@ def extract_attention_local(head, annotated_tokens):
             global_att.append(sum(global_att_weight))
             local_att_wieght=[]
             global_att_weight=[]
-            # recieved_sum = get_col(head, i)
-            # produced_weights.append(produced_sum/len(annotated_tokens))
-            # recieved_weights.append(recieved_sum/len(annotated_tokens))
     return {'labels': labels, 'analysis1': local_att, 'analysis2': global_att}
 
 
@@ -423,7 +396,6 @@ def remove_padding(layer, tokens):
 
 
 def get_labels_for_tokens(features, layers, focus):
-        # TODO pass the focus word here and change this to be included into the focus, make sure you replace the tokens with the annotated tokens
         o_points = []
         loc_points = []
         org_points = []
